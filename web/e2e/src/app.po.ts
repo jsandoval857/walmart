26169d300a0b451ce5d037b297482a986c67e370
José A Sandoval C <jsandoval857@gmail.com>
2020-07-18 21:32:02 -0400

    Arreglo de Dokerfile y errores de pruebas

--- web/e2e/src/app.po.ts
+++ web/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root .main-container p.texto')).getText() as Promise<string>;
   }
 }
